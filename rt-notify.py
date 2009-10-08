0913ebece9236f82eddcd56a8b8648bdb86fd2d4
Ross Macduff <rmacduff@chass.utoronto.ca>
2009-10-08 12:29:56 -0400

    fixed up issues with keeping state

--- rt-notify.py
+++ rt-notify.py
@@ -11,12 +11,13 @@ import re
 import time
 
 QUERY = "\"Owner=\'Nobody\' AND ( Status=\'new\' OR Status=\'open\' ) AND ( Queue!=\'spambin\' AND Queue!=\'maildrop\' AND Queue!=\'learnspam\' )\""
-TIME = 60
+TIME = 300 # in seconds
 KEEP_STATE = True
 RT_CLI = "/usr/bin/rt-3.6"
 
 
 seen_queue = []
+out_queue = []
 
 if __name__ == '__main__':
     if not pynotify.init("Urgency"):
@@ -28,7 +29,7 @@ if __name__ == '__main__':
     while True:
         output = subprocess.Popen(rt_cmd, shell=True, stdout=subprocess.PIPE).communicate()[0]
 
-        # Nothing if it returns "No matching results."
+        # Do nothing if it returns "No matching results."
         pat = "^No matching results."
         match = re.match(pat, output)
 
@@ -38,20 +39,29 @@ if __name__ == '__main__':
                     pass
 
                 ticket_id = line.split(':')[0]
-                print line, ticket_id
 
-                if KEEP_STATE and ticket_id not in seen_queue:
-                    seen_queue.append(ticket_id)
+                if KEEP_STATE:
+                    if ticket_id not in seen_queue:
+                        seen_queue.append(ticket_id)
+                        out_queue.append(line)
+                        # keep the seen queue from growing too large (improve this)   
+                        if len(seen_queue) > 50:
+                            seen_queue.pop(0)
+                    else:
+                        pass
+                else:
+                    out_queue.append(line)
+
+                #done for loop
+
+            if len(out_queue) != 0:
+                n = pynotify.Notification("RT Notice", '\n'.join(out_queue), rt_img)
+                n.set_urgency(pynotify.URGENCY_LOW)
             
-                    n = pynotify.Notification("RT Notice", output, rt_img)
-                    n.set_urgency(pynotify.URGENCY_LOW)
+                if not n.show():
+                    print "Failed to send notification"
+                    sys.exit(1)
 
-                    if not n.show():
-                        print "Failed to send notification"
-                        sys.exit(1)
-
-                    # keep the seen queue from growing too large (improve this)   
-                    if len(seen_queue) > 50:
-                        seen_queue.pop(0)
+                out_queue = []
             
         time.sleep(TIME)
